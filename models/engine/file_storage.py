8c9926cbd049a9ad63cdc7e36c6c429964992a63
MarkEddy001 <Markeddy254@gmail.com>
2023-12-09 16:04:41 -0800

    Files containig the model folder

---
+++ models/engine/file_storage.py
@@ -0,0 +1,124 @@
+#!/usr/bin/python3
+"""Module designed for the serialization and deserialization purposes of
+BaseModel object for FileStorage class."""
+import datetime
+import json
+import os
+
+
+class FileStorage:
+
+    """Handles the serializtion and deserialization of base classes.
+
+    class 'BaseModel' -> to_dict() <class 'dict'>
+    -> JSON dump <class 'str'> -> FILE <class 'str'>
+    -> JSON load <class 'dict'> -> <class 'BaseModel'
+
+    constructor -> __init__(self)
+    -----------------------------
+
+    Private class attributes
+    ------------------------
+    __file_path: string - path to the JSON file (ex: file.json)
+    __objects: dictionary - empty but will store all objects
+                by <class name>.id as a key
+
+    Public instance methods
+    -----------------------
+    all(self): returns the dictionary __objects
+    new(self, obj): sets in __objects the obj
+                    with key <obj class name>.id
+    save(self): serializes __objects to the JSON file
+                (path: __file_path)
+    reload(self): deserializes the JSON file to __objects
+                    (only if the JSON file (__file_path) exists ;
+                    otherwise, no exception should be raised)
+    """
+
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """Returns the __objects dictionary."""
+        # TODO: should this be a copy()?
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """Sets the new obj in __objects dictionary."""
+        # TODO: should these be more precise specifiers?
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        """Serialzes the __objects to the JSON file."""
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
+            d = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
+            json.dump(d, f)
+
+    def classes(self):
+        """Provides a dictionary of valid classes and their references."""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
+
+        classes = {"BaseModel": BaseModel,
+                   "User": User,
+                   "State": State,
+                   "City": City,
+                   "Amenity": Amenity,
+                   "Place": Place,
+                   "Review": Review}
+        return classes
+
+    def reload(self):
+        """Deserializes/ converts the JSON file into __objects."""
+        if not os.path.isfile(FileStorage.__file_path):
+            return
+        with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
+            obj_dict = json.load(f)
+            obj_dict = {k: self.classes()[v["__class__"]](**v)
+                        for k, v in obj_dict.items()}
+            # TODO: should this overwrite or insert?
+            FileStorage.__objects = obj_dict
+
+    def attributes(self):
+        """Retrives and returns the valid attributes and their types for classname."""
+        attributes = {
+            "BaseModel":
+                     {"id": str,
+                      "created_at": datetime.datetime,
+                      "updated_at": datetime.datetime},
+            "User":
+                     {"email": str,
+                      "password": str,
+                      "first_name": str,
+                      "last_name": str},
+            "State":
+                     {"name": str},
+            "City":
+                     {"state_id": str,
+                      "name": str},
+            "Amenity":
+                     {"name": str},
+            "Place":
+                     {"city_id": str,
+                      "user_id": str,
+                      "name": str,
+                      "description": str,
+                      "number_rooms": int,
+                      "number_bathrooms": int,
+                      "max_guest": int,
+                      "price_by_night": int,
+                      "latitude": float,
+                      "longitude": float,
+                      "amenity_ids": list},
+            "Review":
+            {"place_id": str,
+                         "user_id": str,
+                         "text": str}
+        }
+        return attributes
